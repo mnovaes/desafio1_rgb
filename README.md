f892da249bb5c01d37f4502b0a439a4a4e78a0c5
matheus novaes <summerteeth1985@gmail.com>
2020-05-19 21:49:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desafio1_rgb
+ Construa, utilizando HTML, CSS e JavaScript puro, uma aplicação para a visualização de cores a partir da escala RGB.
---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('load', start);
+
+function refreshText() {
+  let i = 0;
+  while (i < 2) {
+    label[i].value = arrayRgb[i].value;
+    i++;
+  }
+}
+
+function start() {}
+console.log('tudo pronto');
+var rgb = document.querySelectorAll('input.slider');
+var arrayRgb = Array.from(rgb);
+var label = document.querySelectorAll('input.txt');
+var arrayLabel = Array.from(label);
+let colorBox = document.querySelector('.color-box');
+console.log(arrayRgb[0].value);
+console.log(arrayLabel[0].value);
+
+window.addEventListener('change', (event) => {
+  switch (event.target.id) {
+    case '#redLabel':
+      arrayRgb[0].value = event.target.value;
+      break;
+    case '#GreenLabel':
+      arrayRgb[1].value = event.target.value;
+      break;
+    case '#blueLabel':
+      arrayRgb[2].value = event.target.value;
+      break;
+  }
+  refreshText();
+  colorBox.style.backgroundColor = `rgb(${arrayRgb[0].value},${arrayRgb[1].value},${arrayRgb[2].value})`;
+});
